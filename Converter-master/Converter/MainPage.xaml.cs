7b067c1df07a3848eaf822a12bc2671fa2d8967b
imp0liter <60029397+imp0liter@users.noreply.github.com>
2021-06-07 12:28:46 +0300

    Converter

---
+++ Converter-master/Converter/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Converter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public double GBP(double с)
+        {
+            double k = с * 0.7085;
+            return k;
+        }
+        public void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = GBP(Convert.ToDouble(First.Text));
+                double result = a;
+
+                Page1 secondpage = new Page1();
+                Navigation.PushAsync(secondpage);
+                secondpage.Calculator(result);
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Ошибка", "Ошибка введенных данных!", "Ввести заново");
+            }
+        }
+    }
+
+}
+
+
